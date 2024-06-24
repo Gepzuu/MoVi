21eb143987da826ea610c2e8855e99a0b897b797
Gep <johnmicgepulla@gmail.com>
2024-06-24 20:14:49 +0800

    Added keyboard navigation to Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,21 @@
 import "./Header.css";
 
 const Header = () => {
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter' || event.key === ' ') {
+      window.scroll(0, 0);
+    }
+  };
+
   return (
-    <span onClick={() => window.scroll(0, 0)} className="header">
- 
+    <span
+      onClick={() => window.scroll(0, 0)}
+      onKeyDown={handleKeyDown}
+      role="button"
+      tabIndex={0}
+      className="header"
+    >
+      {/* Add any content or text for the header here */}
     </span>
   );
 };
